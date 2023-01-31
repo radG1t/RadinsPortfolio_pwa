b284fc83b006e60a70ab219b260d6ce5007fee12
radin <radinErfanfar@gmail.com>
2023-02-01 01:27:54 +0330

    page2 added.

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_portfolio/resume.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -7,22 +10,102 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /* appBar: AppBar(
+        // Appbar may be deleted.
         toolbarHeight: 48,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Text('Radins Portfolio'),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.62,
-            ),
             IconButton(
               onPressed: () {},
               icon: const Icon(CupertinoIcons.moon,
                   color: Colors.white, size: 22),
-            )
+            ),
           ],
+        ),
+      ),  */
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Radin Erfanfar',
+                  style: GoogleFonts.lato(
+                    fontSize: 38,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Junior Flutter Developer.',
+                  style: GoogleFonts.lato(
+                    fontSize: 38,
+                    color: Colors.black,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'email.png',
+                      width: 28,
+                    ),
+                    Text(' RadinErfanfar@gmail.com'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'phone.png',
+                      width: 28,
+                    ),
+                    const Text('+98 9902532818'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'github.png',
+                      width: 48,
+                    ),
+                    Image.asset(
+                      'linkedin.png',
+                      width: 48,
+                    ),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ResumerPage()),
+                    ),
+                    child: Container(
+                      color: Colors.black,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Open my resuome',
+                          style: GoogleFonts.lato(
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/resume.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ResumerPage extends StatelessWidget {
+  const ResumerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Resume')),
+    );
+  }
+}
